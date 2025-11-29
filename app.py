82d3f8a75ea108fa6554a9b8d4bc8a0560d1c9cc
CEYA33 <celesteybs19@gmail.com>
2025-11-28 20:28:18 -0500

    Refactor routes and database connection in app.py
    Refactor app.py to change 'registro' routes to 'resena', update database connection details, and remove unused pusher functionality.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, render_template, request, jsonify, make_response, sessi
 from flask_cors import CORS, cross_origin
 
 import mysql.connector.pooling
-import pusher
+# import pusher
 import pytz
 import datetime
 
@@ -21,9 +21,9 @@ con_pool = mysql.connector.pooling.MySQLConnectionPool(
     pool_name="my_pool",
     pool_size=5,
     host="185.232.14.52",
-    database="u760464709_prueba_bd",
-    user="u760464709_prueba_usr",
-    password="FnlRDqu3@A"
+    database="u760464709_22005483_bd",
+    user="u760464709_22005483_usr",
+    password="XHIUfrIjh7|"
 )
 
 
@@ -35,12 +35,11 @@ def manifest():
 def pwaSW():
     return app.send_static_file("pwa-sw.js")
 
-
-def pusherModulo():
-    pusher_client = pusher.Pusher()
+# def pusherModulo():
+#     pusher_client = pusher.Pusher()
     
-    pusher_client.trigger("canalModulos", "eventoModulo", {})
-    return make_response(jsonify({}))
+#     pusher_client.trigger("canalModulos", "eventoModulo", {})
+#     return make_response(jsonify({}))
 
 def login(fun):
     @wraps(fun)
@@ -67,22 +66,21 @@ def appLogin():
     return render_template("login.html")
     # return "<h5>Hola, soy la view app</h5>"
 
-@app.route("/registros")
-def registros():
-    return render_template("registros.html")
+@app.route("/resenas")
+def resenas():
+    return render_template("resenas.html")
 
-@app.route("/registro")
-def registro():
-    return render_template("registro.html")
+@app.route("/resena")
+def resena():
+    return render_template("resena.html")
 
-@app.route("/notificaciones")
-def notificaciones():
-    return render_template("notificaciones.html")
+# @app.route("/notificaciones")
+# def notificaciones():
+#     return render_template("notificaciones.html")
 
 @app.route("/offline")
 def offline():
     return render_template("offline.html")
-
 
 @app.route("/ping")
 def ping():
@@ -112,7 +110,7 @@ def iniciarSesion():
     val    = (usuario, contrasena)
 
     cursor.execute(sql, val)
-    registros = cursor.fetchall()
+    usuarios = cursor.fetchall()
     if cursor:
         cursor.close()
     if con and con.is_connected():
@@ -121,13 +119,13 @@ def iniciarSesion():
     session["login2"]     = False
     session["login2-id"]  = None
     session["login2-usr"] = None
-    if registros:
-        usuario = registros[0]
+    if usuarios:
+        usuario = usuarios[0]
         session["login2"]     = True
         session["login2-id"]  = usuario["id"]
         session["login2-usr"] = usuario["usuario"]
 
-    return make_response(jsonify(registros))
+    return make_response(jsonify(usuarios))
 
 @app.route("/cerrarSesion", methods=["POST"])
 @login
@@ -145,9 +143,9 @@ def preferencias():
     }))
 
 
-@app.route("/registros/buscar", methods=["GET"])
+@app.route("/resenas/buscar", methods=["GET"])
 @login
-def buscarRegistros():
+def buscarResenas():
     args     = request.args
     busqueda = args["busqueda"]
     busqueda = f"%{busqueda}%"
@@ -157,20 +155,23 @@ def buscarRegistros():
         cursor = con.cursor(dictionary=True)
         sql    = """
         SELECT id,
-            descripcion,
-            fechaHora
-        FROM registros
-        WHERE descripcion LIKE %s
+            nombre,
+            restaurante,
+            direccion,
+            resena,
+            calificacion
+        FROM resenas_comidas
+        WHERE nombre LIKE %s
         ORDER BY id DESC
         LIMIT 25 OFFSET 0
         """
         val    = (busqueda, )
 
         cursor.execute(sql, val)
-        registros = cursor.fetchall()
+        resenas = cursor.fetchall()
 
     except mysql.connector.errors.ProgrammingError as error:
-        registros = []
+        resenas = []
 
     finally:
         if cursor:
@@ -178,13 +179,17 @@ def buscarRegistros():
         if con and con.is_connected():
             con.close()
 
-    return make_response(jsonify(registros))
+    return make_response(jsonify(resenas))
 
-@app.route("/registro/guardar", methods=["POST"])
+@app.route("/resena/guardar", methods=["POST"])
 @login
-def guardarRegistro():
+def guardarResena():
     id          = request.form["id"]
-    descripcion = request.form["descripcion"]
+    nombre     = request.form["nombre"]
+    restaurante  = request.form["restaurante"]
+    direccion   = request.form["direccion"]
+    resena = request.form["resena"]
+    calificacion = request.form["calificacion"]
     tz          = pytz.timezone("America/Matamoros")
     ahora       = datetime.datetime.now(tz)
     fechaHora   = ahora.strftime("%Y-%m-%d %H:%M:%S")
@@ -194,17 +199,21 @@ def guardarRegistro():
 
     if id:
         sql = """
-        UPDATE registros
-        SET descripcion = %s
+        UPDATE resenas_comidas
+        SET nombre = %s,
+            restaurante = %s,
+            direccion = %s,
+            resena = %s,
+            calificacion = %s
         WHERE id = %s
         """
-        val = (descripcion, id)
+        val = (nombre, restaurante, direccion, resena, calificacion, id)
     else:
         sql = """
-        INSERT INTO registros (descripcion, fechaHora)
-        VALUES                (%s,          %s)
+        INSERT INTO resenas_comidas (nombre, restaurante, direccion, resena, calificacion)
+        VALUES                (%s, %s, %s, %s, %s)
         """
-        val =                 (descripcion, fechaHora)
+        val =                 (nombre, restaurante, direccion, resena, calificacion)
     
     cursor.execute(sql, val)
     con.commit()
@@ -222,35 +231,35 @@ def guardarRegistro():
         "fechaHora": fechaHora
     }))
 
-@app.route("/registro/<int:id>")
+@app.route("/resena/<int:id>")
 @login
-def editarRegistro(id):
+def editarResena(id):
     con    = con_pool.get_connection()
     cursor = con.cursor(dictionary=True)
     sql    = """
-    SELECT id, descripcion, fechaHora
-    FROM registros
+    SELECT id, nombre, restaurante, direccion, resena, calificacion
+    FROM resenas_comidas
     WHERE id = %s
     """
     val    = (id,)
 
     cursor.execute(sql, val)
-    registros = cursor.fetchall()
+    resenas = cursor.fetchall()
     if cursor:
         cursor.close()
     if con and con.is_connected():
         con.close()
 
-    return make_response(jsonify(registros))
+    return make_response(jsonify(resenas))
 
-@app.route("/registro/eliminar", methods=["POST"])
-def eliminarRegistro():
+@app.route("/resena/eliminar", methods=["POST"])
+def eliminarResena():
     id = request.form["id"]
 
     con    = con_pool.get_connection()
     cursor = con.cursor(dictionary=True)
     sql    = """
-    DELETE FROM registros
+    DELETE FROM resenas_comidas
     WHERE id = %s
     """
     val    = (id,)
@@ -263,54 +272,6 @@ def eliminarRegistro():
         con.close()
 
     return make_response(jsonify({}))
-
-
-@app.route("/notificaciones/cargar", methods=["GET"])
-@login
-def cargarNotificaciones():
-    # args     = request.args
-    # busqueda = args["busqueda"]
-    # busqueda = f"%{busqueda}%"
-
-    try:
-        con    = con_pool.get_connection()
-        cursor = con.cursor(dictionary=True)
-        sql    = """
-        SELECT id,
-            titulo,
-            contenido,
-            fechaHora,
-            `READ`
-        FROM notificaciones
-        WHERE notificaciones.usuario = %s
-        ORDER BY id DESC
-        LIMIT 25 OFFSET 0
-        """
-        val    = (session.get("login2-id"), )
-
-        cursor.execute(sql, val)
-        registros = cursor.fetchall()
-
-    except mysql.connector.errors.ProgrammingError as error:
-        registros = []
-
-    finally:
-        if cursor:
-            cursor.close()
-        if con and con.is_connected():
-            con.close()
-
-    return make_response(jsonify(registros))
-
-@app.route("/notificacion/eliminar", methods=["POST"])
-@login
-def eliminarNotificacion():
-    return "eliminar"
-
-@app.route("/notificacion/marcarComoLeida", methods=["POST"])
-@login
-def marcarNotificacionComoLeida():
-    return "leida"
 
 if __name__ == "__main__":
     app.run()
